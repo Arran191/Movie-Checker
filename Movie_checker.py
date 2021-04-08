1ee906ca03247bc6ccc267c1ff37596251458031
Arran191 <32872227+Arran191@users.noreply.github.com>
2021-04-08 22:02:31 +0100

    Added colours and refined additional bar.

--- Movie_checker.py
+++ Movie_checker.py
@@ -1,4 +1,3 @@
-
 import sys
 import urllib
 import argparse
@@ -7,16 +6,24 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import pandas as pd
+from colorama import Fore, Back, Style, init
 
 
-def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
+def printProgressBar(
+    iteration,
+    total,
+    prefix="",
+    suffix="",
+    decimals=1,
+    length=100,
+    fill="█",
+    printEnd="\r",
+):
 
-    percent = ("{0:." + str(decimals) + "f}").format(100 *
-                                                     (iteration / float(total)))
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print(
-        f'\r{prefix} |{bar}|{percent}% {suffix}', end=printEnd)
+    bar = fill * filledLength + "-" * (length - filledLength)
+    print(Style.RESET_ALL + f"\r{prefix} |{bar}|{percent}% {suffix}", end=printEnd)
     # Print New Line on Complete
     if iteration == total:
         print()
@@ -24,7 +31,7 @@ def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=
 
 def find_platform(url):
     platforms = ["netflix", "disneyplus", "amazon", "youtube", "play.google"]
-    #print("URLS" + url)
+    # print("URLS" + url)
     results = [element for element in platforms if (element in url)]
 
     if results:
@@ -41,13 +48,14 @@ def find_platform(url):
     else:
         return "no results"
 
+
 # Default = movie cause if you need to Google for the year, kinda defies the point...
 
 
 def check_movie(query, year="movie"):
-    query = query.replace(' ', '+')
+    query = query.replace(" ", "+")
     URL = f"https://google.com/search?q={query}+{year}"
-    #print("Checking: " + URL)
+    # print("Checking: " + URL)
 
     USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
     headers = {"user-agent": USER_AGENT}
@@ -56,52 +64,59 @@ def check_movie(query, year="movie"):
     if resp.status_code == 200:
         soup = BeautifulSoup(resp.content, "html.parser")
 
-    for g in soup.find_all('div', class_='fOYFme'):
-        anchors = g.find_all('a')
-        #checkPaid = soup.select('.uiBRm')
+    for g in soup.find_all("div", class_="fOYFme"):
+        anchors = g.find_all("a")
+        # checkPaid = soup.select('.uiBRm')
         checkPaid = g.find("div", {"class": "uiBRm"})
-        #print("Checkpaid" + str(checkPaid))
+        # print("Checkpaid" + str(checkPaid))
         if anchors and checkPaid:
+            # Checks if it is a premium subscription that prime has. 
             if checkPaid.text.find("Subscription"):
-                #print(f"{query} Paid movie {checkPaid.text}")
+                # print(f"{query} Paid movie {checkPaid.text}")
                 return "paid"
             else:
-                #print(f"{query} Subscribed movie {checkPaid.text}")
-                return anchors[0]['href']
+                # print(f"{query} Subscribed movie {checkPaid.text}")
+                return anchors[0]["href"]
 
     return "No results"
 
 
 def check_csv_list(compare=False, ignore=False):
-    df = pd.read_csv('WatchingList.csv')
+    df = pd.read_csv("WatchingList.csv")
     length = len(df)
-
     for index, movie in df.Name.iteritems():
-        filledLength = int(50 * index // length)
-        bar = "-" * filledLength
-        printProgressBar(index + 1, length, prefix='Progress:',
-                         suffix='Complete', length=50)
-
+        # filledLength = int(50 - (index + 1) // length)
+        printProgressBar(
+            index + 1, length, prefix="Progress:", suffix="Complete", length=50
+        )
 
         # Ignore the movie if already watched.
-        if df.Status[index] == "Complete" or df.Location[index] == "Shudder" or df.Location[index] == "Tom-Paid":
+        if (
+            df.Status[index] == "Complete"
+            or df.Location[index] == "Shudder"
+            or df.Location[index] == "Tom-Paid"
+        ):
             continue
 
         year = int(df.Year[index])
         linkResults = check_movie(movie, year)
         platform = find_platform(linkResults)
+        sentence = ""
 
         if compare == False and platform != "no results":
-            print(
-                f"{movie} can be viewed on {platform.upper()}{bar}\r")
+            sentence = f"{movie} can be viewed on {platform.upper()}"
 
         if str(df.Location[index]).lower() != platform:
             if platform == "no results" and ignore == False:
-                print(
-                    f"{movie} {year} has {platform.upper()}{bar}\r")
+                sentence = f"{movie} {year} has {platform.upper()}"
             elif platform != "no results":
-                print(
-                    f"{movie} {year} has changed from {df.Location[index]} to {platform.upper()}{bar}\r ")
+                sentence = (
+                    Fore.GREEN
+                    + f"{movie} {year} has changed from {df.Location[index]} to {platform.upper()}"
+                )
+        if sentence != "":
+            bar = "-" * (60 - len(sentence))
+            print(sentence + Fore.RESET + bar + "\r")
 
 
 def main(query, readcsv, isolate, ignore):
@@ -118,23 +133,30 @@ def main(query, readcsv, isolate, ignore):
 
 # For command line.
 def str2bool(value):
-    if value.lower() in {'false', 'f', '0', 'no', 'n'}:
+    if value.lower() in {"false", "f", "0", "no", "n"}:
         return False
-    elif value.lower() in {'true', 't', '1', 'yes', 'y'}:
+    elif value.lower() in {"true", "t", "1", "yes", "y"}:
         return True
-    raise ValueError(f'{value} is not a valid boolean value')
+    raise ValueError(f"{value} is not a valid boolean value")
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prefix_chars='-')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prefix_chars="-")
     parser.add_argument(
-        '-csv', help='Pass in bool value if you want to read the csv', type=str2bool, default=True)
+        "-csv",
+        help="Pass in false to ignore CSV file",
+        type=str2bool,
+        default=True,
+    )
     parser.add_argument(
-        '-q', help='The search term to query if you are not reading from the CSV', default="")
+        "-q",
+        help="The search term to query if you are not reading from the CSV",
+        default="",
+    )
     parser.add_argument(
-        '-o', help='Only show movies that have changed.', type=str2bool, default=False)
-    parser.add_argument(
-        '-i', help='Ignore no results', type=str2bool, default=False)
+        "-o", help="Only show movies that have changed.", type=str2bool, default=False
+    )
+    parser.add_argument("-i", help="Ignore no results", type=str2bool, default=False)
     args = parser.parse_args()
-
+    init()
     main(args.q, args.csv, args.o, args.i)
